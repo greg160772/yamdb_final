06b7d222c112687d900ea585708ccbd3b57f63ab
Grigory Alexndrov <greg160772@gmail.com>
2021-05-03 18:59:20 +0300

    Change base layer for yatube_fimal image

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -10,7 +10,12 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "130.193.53.107",
+    "testserver",
+]
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
